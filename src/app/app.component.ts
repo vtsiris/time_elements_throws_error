aacc350042230fe0e297c45b3ab59dc1ccfd5776
Tsiris Vaggelis <tsirisvaggelis@gmail.com>
2020-10-14 13:11:05 +0300

    reverted some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
-import {AfterViewInit, Component} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'test-time';
   time = '19-08-2020';
-
-  constructor(private http: HttpClient) {
-
-  }
-
-  ngAfterViewInit() {
-    const options = {
-      observe: 'response' as 'body',
-      headers: new HttpHeaders({
-        Authorization: 'Bearer AwdasdsadSomeToken',
-        'Content-Type': 'application/json',
-        Accept: 'application/json,application/vnd.error+json'
-      })
-    };
-
-    console.log('Starting http xhr request');
-    this.http.get('http://127.0.0.1:8009/data', options).subscribe((data) => {
-      console.log('Got data ', data);
-    });
-  }
 }
