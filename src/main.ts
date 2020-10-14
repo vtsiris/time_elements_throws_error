47bc19e7a1bd5869d628dcb74cf0fb4c32ca1a23
Tsiris Vaggelis <tsirisvaggelis@gmail.com>
2020-10-14 17:04:26 +0300

    add css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './polyfills';
 // jasmine staff
 declare var jasmine;
 import jasmineRequire from 'jasmine-core/lib/jasmine-core/jasmine.js';
+
 window['jasmineRequire'] = jasmineRequire;
 import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
 import 'jasmine-core/lib/jasmine-core/boot.js';
@@ -17,7 +18,7 @@ import 'zone.js/dist/zone-testing'; // instead all commented files below
 // import 'zone.js/dist/jasmine-patch';
 
 // import testBed and Angular staff
-import { getTestBed } from '@angular/core/testing';
+import {getTestBed} from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
@@ -35,7 +36,7 @@ function bootstrap() {
   // window['jasmineRef'] does nothing - it is just a flag
   // if it is not defined - we have clear run
   // if not - lets reload
-  if (window.jasmineRef) {
+  if (window['jasmineRef']) {
     location.reload();
     return;
   } else {
